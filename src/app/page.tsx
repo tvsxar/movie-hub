1ac47ef42866b025c870343152a4d4259303b54d
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-13 11:22:09 +0200

    feat: setup MoviesPage UI (grid, cards, mock data, layout and fonts)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import MoviesGrid from '@/components/MoviesGrid';
+
 export default function MoviesPage() {
   return (
-    <div className=""></div>
+    <div className="py-6">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-12 lg:px-25">
+        <MoviesGrid />
+      </div>
+    </div>
   );
 }
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import formatDate from '@/lib/utils';
+
+interface Movie {
+    id: number;
+    title: string;
+    release_date: string;
+    poster_path: string
+    vote_average: number;
+}
+
+interface MovieCardProps {
+    movie: Movie
+}
+
+export default function MovieCard({ movie }: MovieCardProps) {
+    return (
+        <div className="group
+          rounded-xl
+          border border-zinc-500/50
+          w-full max-w-xl
+          transition
+          hover:-translate-y-1
+          hover:shadow-[0_10px_30px_rgba(158,188,158,0.25)]">
+            <div className="">
+                <div className="relative aspect-2/3 overflow-hidden rounded-t-xl">
+                    <Image
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                        fill
+                        className="object-cover transition-transform duration-300 group-hover:scale-101"
+                    />
+
+                    <div
+                        className="
+    absolute inset-0
+    bg-linear-to-t
+    from-black/60 via-black/20 to-transparent
+    opacity-0
+    group-hover:opacity-100
+    transition-opacity
+  "
+                    />
+                </div>
+
+                <div className="p-4 flex flex-col gap-4">
+                    <h2 className='font-semibold text-lg text-gray-100 line-clamp-2'>{movie.title}</h2>
+
+                    <div className="flex justify-between text-sm text-gray-400">
+                        <p>{formatDate(movie.release_date)}</p>
+
+                        <p className="text-[#9ebc9e] font-medium">
+                            ★ {movie.vote_average}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
