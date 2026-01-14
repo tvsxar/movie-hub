d7a9598673a88376f0fa32c1add174cda87bdcf0
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-14 12:06:42 +0200

    feat: add empty state for MoviesPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import MoviesGrid from '@/components/MoviesGrid';
 import SearchBar from '@/components/SearchBar';
 import Pagination from '@/components/Pagination';
+import EmptyState from '@/components/EmptyState';
 import { getMovies } from "@/lib/actions";
 
 interface MoviesPageProps {
@@ -14,15 +15,19 @@ export default async function MoviesPage({ searchParams }: MoviesPageProps) {
   const resolvedParams = await searchParams;
 
   const page = Number(resolvedParams.page) || 1;
-  const searchQuery = resolvedParams.search || ""; 
+  const searchQuery = resolvedParams.search || "";
   const data = await getMovies(page, searchQuery);
 
   return (
     <div className="py-6">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-12 lg:px-25">
         <SearchBar initialValue={searchQuery} />
-        <MoviesGrid movies={data.results} />
-        <Pagination currentPage={data.page} totalPages={data.total_pages} />
+        {data.results.length === 0
+          ? <EmptyState />
+          : <>
+            <MoviesGrid movies={data.results} />
+            <Pagination currentPage={data.page} totalPages={data.total_pages} />
+          </>}
       </div>
     </div>
   );
