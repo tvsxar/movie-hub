b0ede4cedaeb37b795b4689c3d38887ef2b3a356
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-16 12:28:20 +0200

    feat: wrapped layout with provider & added loading state and error handling in hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from "@/context/useAuth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <Navbar />
+        <AuthProvider>
+          <Navbar />
 
-        <main className="flex-1 flex flex-col bg-[#0f0f0f]">{children}</main>
+          <main className="flex-1 flex flex-col bg-[#0f0f0f]">{children}</main>
 
-        <Footer />
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
--- src/context/useAuth.tsx
+++ src/context/useAuth.tsx
@@ -18,26 +18,31 @@ interface AuthContextType {
     user: UserType | null,
     logout: () => Promise<void>;
     auth: (isLogin: boolean, userData: UserDataType) => Promise<void>;
+    loading: boolean
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
     const [user, setUser] = useState<UserType | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         async function getUser() {
             try {
+                setLoading(true);
                 const response = await fetch('/api/auth/me', {
                     cache: 'no-store',
                 });
                 const data = await response.json();
                 setUser(data.user);
+                setLoading(false);
             } catch (error) {
                 if (error instanceof Error) {
                     console.error(error.message);
                 }
                 setUser(null);
+                setLoading(false);
             }
         }
 
@@ -46,15 +51,19 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
 
     async function logout() {
         try {
+            setLoading(true);
             const response = await fetch('/api/auth/logout', { method: 'POST', cache: 'no-store' });
             setUser(null);
+            setLoading(false);
         } catch (error) {
             console.error(error);
+            setLoading(false);
         }
     }
 
     async function auth(isLogin: boolean, userData: { login?: string, email?: string, username?: string, password: string }) {
         try {
+            setLoading(true);
             const response = await fetch(isLogin ? '/api/auth/login' : '/api/auth/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -63,16 +72,22 @@ export default function AuthProvider({ children }: { children: React.ReactNode }
             });
             const data = await response.json();
 
+            if (!response.ok) {
+                throw new Error(data.message || 'Authentication failed');
+            }
+
             setUser(data.user);
+            setLoading(false);
         } catch (error) {
-            console.error(error);
+            setLoading(false);
+            throw error;
         }
     }
 
-    return <AuthContext.Provider value={{ user, logout, auth }}>{children}</AuthContext.Provider>
+    return <AuthContext.Provider value={{ user, logout, auth, loading }}>{children}</AuthContext.Provider>
 }
 
-export function useAuth() {
+export function useAuth(): AuthContextType {
     const context = useContext(AuthContext);
 
     if (!context) {
