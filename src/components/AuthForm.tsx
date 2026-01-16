1265abddd0b7c0c6cc4bfc7be7fa964ae41b6843
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-16 12:31:56 +0200

    feat: integrate auth functionality and error display

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,23 +1,100 @@
+'use client'
+import { useAuth } from '@/context/useAuth';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 interface AuthFormProps {
     isLogin: boolean;
 }
 
 export default function AuthForm({ isLogin }: AuthFormProps) {
+    const { auth } = useAuth();
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+    const [userData, setUserData] = useState({
+        login: '',
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    useEffect(() => {
+        setUserData({
+            login: '',
+            username: '',
+            email: '',
+            password: ''
+        });
+    }, [isLogin]);
+
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target;
+
+        setUserData(prev => ({ ...prev, [name]: value }));
+    }
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setError(null);
+
+        const dataToSend = { ...userData };
+
+        if (isLogin) {
+            dataToSend.email = '';
+            dataToSend.username = '';
+        } else {
+            dataToSend.login = '';
+        }
+
+        try {
+            await auth(isLogin, dataToSend);
+            router.push('/');
+
+            setUserData({
+                login: '',
+                username: '',
+                email: '',
+                password: ''
+            });
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            }
+        }
+    }
+
     return (
-        <form className="flex flex-col gap-4">
-            {!isLogin &&
-                <input
-                    type="text"
-                    placeholder="Username"
-                    className="w-full px-4 py-3 rounded-lg bg-zinc-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#9ebc9e] transition"
-                />}
+        <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4">
+
+            {error && (
+                <p className="text-red-400 text-sm text-center">
+                    {error}
+                </p>
+            )}
+
             <input
-                type="email"
-                placeholder="Email"
+                type="text"
+                name={isLogin ? 'login' : 'username'}
+                required
+                onChange={handleInput}
+                placeholder={isLogin ? "Username or Email" : "Username"}
                 className="w-full px-4 py-3 rounded-lg bg-zinc-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#9ebc9e] transition"
             />
+            {!isLogin && <input
+                type="email"
+                name="email"
+                required
+                onChange={handleInput}
+                placeholder="Email"
+                className="w-full px-4 py-3 rounded-lg bg-zinc-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#9ebc9e] transition"
+            />}
             <input
                 type="password"
+                name="password"
+                required
+                onChange={handleInput}
                 placeholder="Password"
                 className="w-full px-4 py-3 rounded-lg bg-zinc-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#9ebc9e] transition"
             />
@@ -26,7 +103,7 @@ export default function AuthForm({ isLogin }: AuthFormProps) {
                 type="submit"
                 className="w-full py-3 mt-2 bg-[#9ebc9e] text-black font-semibold rounded-lg hover:bg-[#8aad8a] transition cursor-pointer"
             >
-                Sign Up
+                {isLogin ? 'Sign In' : 'Sign Up'}
             </button>
         </form>
     )
