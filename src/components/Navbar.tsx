e55dda9d992ad2f353c30497105b2027de345bed
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-16 12:34:50 +0200

    feat: logout functionality & skeleton

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,26 @@
+'use client'
 import Link from 'next/link';
 import AuthButtons from '@/components/AuthButtons';
 import UserButtons from '@/components/UserButtons';
+import { useAuth } from '@/context/useAuth';
 
 export default function Navbar() {
-    const mockUsername = 'username';
+    const { user, logout, loading } = useAuth();
 
     return (
         <nav
             className="flex select-none items-center justify-between 
             border-b border-gray-100/30 py-4 px-4 sm:px-12 lg:px-25">
+
             <Link href="/" className='font-bold text-lg sm:text-2xl flex items-center justify-center'>
                 Movie<span className="text-[#9ebc9e] drop-shadow-[0_0_4px_rgba(158,188,158,0.6)]">Hub</span>
             </Link>
 
-            {true ? <AuthButtons /> : <UserButtons username={mockUsername} />}
+            {loading
+                ? <div className="h-8 w-32 bg-zinc-800 rounded animate-pulse" />
+                : user
+                    ? <UserButtons username={user.username} logout={logout} />
+                    : <AuthButtons />}
         </nav>
     )
 }
--- src/components/UserButtons.tsx
+++ src/components/UserButtons.tsx
@@ -3,30 +3,32 @@ import { HiLogout } from 'react-icons/hi';
 import { FiBookmark } from 'react-icons/fi';
 
 interface UserButtonsProps {
-    username: string
+    username: string,
+    logout: () => Promise<void>;
 }
 
-function UserButtons({ username }: UserButtonsProps) {
+function UserButtons({ username, logout }: UserButtonsProps) {
     return (
-        <div className='flex gap-4'>
+        <div className='flex gap-3 sm:gap-4'>
 
-            <div className="hidden sm:flex items-center gap-2">
-                <div className="h-8 w-8 flex items-center justify-center rounded-full
-                 bg-gray-700 text-gray-100 font-semibold text-sm">{username[0]}</div>
-                <p className='font-semibold'>{username}</p>
+            <div className="flex items-center gap-2">
+                <div className="w-9 h-9 sm:h-8 sm:w-8 flex items-center justify-center rounded-full
+                 bg-gray-700 text-gray-100 font-semibold text-sm">{username[0].toUpperCase()}</div>
+                <p className='font-semibold hidden sm:flex'>{username}</p>
             </div>
 
             <div className="flex items-center gap-2">
                 <Link href="/favourites" className="cursor-pointer bg-[#9ebc9e] rounded-xl 
                 p-2 hover:bg-[#9ebc9e]/70 active:scale-97 text-black transition 
                 hover:drop-shadow-[0_0_4px_rgba(158,188,158,0.6)]">
-                    <FiBookmark className="w-6 h-6" />
+                    <FiBookmark className="h-5 w-5 sm:w-6 sm:h-6" />
                 </Link>
 
-                <button className='cursor-pointer bg-red-400/80 rounded-xl p-2
+                <button onClick={logout}
+                className='cursor-pointer bg-red-400/80 rounded-xl p-2
                  hover:bg-red-400/50 hover:drop-shadow-[0_0_6px_rgba(239,68,68,0.7)] 
                  active:scale-97 transition'>
-                    <HiLogout className="w-6 h-6" />
+                    <HiLogout className="h-5 w-5 sm:w-6 sm:h-6" />
                 </button>
             </div>
         </div>
