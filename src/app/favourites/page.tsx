dbe8b0b3fdee0dc5b5cfeba31e17d2c6c2e73af2
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-01-19 12:18:58 +0200

    feat: wrapped layout into FavouritesProvider & implement receiving movies functionality on FavouritesPage

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,21 +1,56 @@
+'use client'
 import MoviesGrid from '@/components/MoviesGrid';
 import FavouritesHeader from '@/components/FavouritesHeader';
 import EmptyState from '@/components/EmptyState';
+import SkeletonGrid from '@/components/SkeletonGrid';
+import { getMoviesByIds } from '@/lib/actions';
+import { useFavourite } from '@/context/useFavourite';
+import { useEffect, useState } from 'react';
 
-const mockMovies = {
-    page: 1,
-    total_pages: 5,
-    total_results: 100,
-    results: [
-
-    ],
-};
+interface Movie {
+    id: number;
+    title: string;
+    release_date: string;
+    poster_path: string;
+    vote_average: number;
+}
 
 export default function FavouritesPage() {
+    const { favourites } = useFavourite();
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        async function fetchFavouriteMovies() {
+            if (favourites.length === 0) {
+                setMovies([])
+                setLoading(false)
+                return
+            }
+
+            try {
+                setLoading(true);
+                const moviesData = await getMoviesByIds(favourites);
+                setMovies(moviesData);
+            } catch (err) {
+                console.error('Failed to fetch favourite movies', err);
+                setMovies([]);
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchFavouriteMovies();
+    }, [favourites])
+
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-8 lg:px-12 xl:px-16 py-8 md:py-12 transition-all duration-300">
-            <FavouritesHeader length={mockMovies.results.length} />
-            {mockMovies.results.length === 0 ? <EmptyState isFav={true} /> : <MoviesGrid movies={mockMovies.results} />}
+            <FavouritesHeader length={movies.length} />
+            {loading
+                ? <SkeletonGrid />
+                : movies.length === 0
+                    ? <EmptyState isFav={true} />
+                    : <MoviesGrid movies={movies} />}
         </div>
     )
 }
